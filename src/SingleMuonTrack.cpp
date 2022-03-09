df7d51a921a9bb1ce1ab44d9b9532df81ee5906d
Raman Sehgal <sc.ramansehgal@gmail.com>
2022-03-09 15:21:41 +0530

    Trying to clear Scintillator objects stored in SingleMuonTrack.

--- src/SingleMuonTrack.cpp
+++ src/SingleMuonTrack.cpp
@@ -26,8 +26,8 @@ ClassImp(ismran::SingleMuonTrack)
 
   SingleMuonTrack::~SingleMuonTrack() {}
 
-  //SingleMuonTrack::SingleMuonTrack(std::vector<std::shared_ptr<ScintillatorBar_F>> vecOfScintBars)
-  SingleMuonTrack::SingleMuonTrack(std::vector<ScintillatorBar_F*> vecOfScintBars)
+  // SingleMuonTrack::SingleMuonTrack(std::vector<std::shared_ptr<ScintillatorBar_F>> vecOfScintBars)
+  SingleMuonTrack::SingleMuonTrack(std::vector<ScintillatorBar_F *> vecOfScintBars)
   {
     fVecOfScintillators = vecOfScintBars;
   }
@@ -36,15 +36,18 @@ ClassImp(ismran::SingleMuonTrack)
   {
     for (unsigned int i = 0; i < smt.GetMuonTrack().size(); i++) {
       fVecOfScintillators.push_back(new ScintillatorBar_F(*(smt.GetMuonTrack()[i])));
-      //fVecOfScintillators.push_back(std::shared_ptr<ScintillatorBar_F>(new ScintillatorBar_F(*(smt.GetMuonTrack()[i]))));
+      // fVecOfScintillators.push_back(std::shared_ptr<ScintillatorBar_F>(new
+      // ScintillatorBar_F(*(smt.GetMuonTrack()[i]))));
     }
   }
 
-  //std::vector<std::shared_ptr<ScintillatorBar_F>> SingleMuonTrack::GetMuonTrack() const { return fVecOfScintillators; }
-  std::vector<ScintillatorBar_F*> SingleMuonTrack::GetMuonTrack() const { return fVecOfScintillators; }
+  // std::vector<std::shared_ptr<ScintillatorBar_F>> SingleMuonTrack::GetMuonTrack() const { return fVecOfScintillators;
+  // }
+  std::vector<ScintillatorBar_F *> SingleMuonTrack::GetMuonTrack() const { return fVecOfScintillators; }
 
-  //bool SingleMuonTrack::CompareBarIndexInScintillator(std::shared_ptr<ScintillatorBar_F> i1, std::shared_ptr<ScintillatorBar_F> i2)
-  bool SingleMuonTrack::CompareBarIndexInScintillator(ScintillatorBar_F *i1, ScintillatorBar_F *i2)
+  // bool SingleMuonTrack::CompareBarIndexInScintillator(std::shared_ptr<ScintillatorBar_F> i1,
+  // std::shared_ptr<ScintillatorBar_F> i2)
+  bool SingleMuonTrack::CompareBarIndexInScintillator(ScintillatorBar_F * i1, ScintillatorBar_F * i2)
   {
     return (i1->GetBarIndex() > i2->GetBarIndex());
   }
@@ -54,11 +57,19 @@ ClassImp(ismran::SingleMuonTrack)
     std::sort(fVecOfScintillators.begin(), fVecOfScintillators.end(), CompareBarIndexInScintillator);
   }
 
-  void SingleMuonTrack::clear() { fVecOfScintillators.clear(); }
+  void SingleMuonTrack::clear()
+  {
+
+    for (unsigned int i = 0; i < size(); i++) {
+      delete fVecOfScintillators[i];
+    }
+    fVecOfScintillators.clear();
+  }
 
   unsigned long int SingleMuonTrack::size() { return fVecOfScintillators.size(); }
 
-  //void SingleMuonTrack::push_back(ScintillatorBar_F * scint) { fVecOfScintillators.push_back(std::shared_ptr<ScintillatorBar_F>(scint)); }
+  // void SingleMuonTrack::push_back(ScintillatorBar_F * scint) {
+  // fVecOfScintillators.push_back(std::shared_ptr<ScintillatorBar_F>(scint)); }
   void SingleMuonTrack::push_back(ScintillatorBar_F * scint) { fVecOfScintillators.push_back(scint); }
 
   unsigned int SingleMuonTrack::NumOfHitsInLayer(unsigned int layerNum)
@@ -113,8 +124,8 @@ ClassImp(ismran::SingleMuonTrack)
   void SingleMuonTrack::Print()
   {
     std::cout << "**************************************************************" << std::endl;
-    //std::vector<std::shared_ptr<ScintillatorBar_F>> scintBarVec = GetMuonTrack();
-    std::vector<ScintillatorBar_F*> scintBarVec = GetMuonTrack();
+    // std::vector<std::shared_ptr<ScintillatorBar_F>> scintBarVec = GetMuonTrack();
+    std::vector<ScintillatorBar_F *> scintBarVec = GetMuonTrack();
     for (unsigned int i = 0; i < scintBarVec.size(); i++) {
       scintBarVec[i]->Print();
     }
